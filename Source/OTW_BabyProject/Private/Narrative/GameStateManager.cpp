5ddbf8bb29dc529a3a017fdef8bf6669d7008831
2300010 <94806803+2300010@users.noreply.github.com>
2025-11-25 13:23:59 -0500

    GameStateManager class POPULATED
    GameStateManager class MODIFIED:
    - TMap Flags, Stats and Relationships variables ADDED
    - Getters and Setters methods CREATED
    - Modify methods (Stats and Relationships) methods CREATED
    - HasStat and ApplyConsequence methods CREATED

--- Source/OTW_BabyProject/Private/Narrative/GameStateManager.cpp
+++ Source/OTW_BabyProject/Private/Narrative/GameStateManager.cpp
@@ -1,3 +1,113 @@
 // © 2025 Open To Work - Samuel Abel
 
 #include "Narrative/GameStateManager.h"
+
+bool AGameStateManager::GetFlag(FName FlagName) const
+{
+	return Flags[FlagName];
+}
+
+float AGameStateManager::GetStat(FName StatName) const
+{
+	return Stats[StatName];
+}
+
+float AGameStateManager::GetRelationship(FName CharacterName) const
+{
+	return Relationships[CharacterName];
+}
+
+void AGameStateManager::SetFlag(FName FlagName, bool Value)
+{
+	Flags[FlagName] = Value;
+}
+
+void AGameStateManager::SetStat(FName StatName, float Value)
+{
+	if (!Value || Value < 0.0f)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("GameStateManager: Trying to set stat with an invalid Value parameter."));
+		return;
+	}
+	Stats[StatName] = Value;
+}
+
+void AGameStateManager::SetRelationship(FName CharacterName, float Value)
+{
+	if (!Value || Value < 0.0f)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("GameStateManager: Trying to set relationship with an invalid Value parameter."));
+		return;
+	}
+	Relationships[CharacterName] = Value;
+}
+
+void AGameStateManager::ModifyStat(FName StatName, float Delta)
+{
+	Stats[StatName] += Delta;
+}
+
+void AGameStateManager::ModifyRelationship(FName CharacterName, float Delta)
+{
+	Relationships[CharacterName] += Delta;
+}
+
+void AGameStateManager::ApplyConsequence(const FChoiceConsequence& Consequence)
+{
+	const EOperation CurrentOperation = Consequence.Operation;
+
+	switch (Consequence.Type)
+	{
+	case EConsequenceType::SetFlag:
+		SetFlag(Consequence.Target, bool(Consequence.Value));
+		break;
+
+	case EConsequenceType::ModifyStat:
+		if (CurrentOperation == EOperation::Set)
+		{
+			SetStat(Consequence.Target, Consequence.Value);
+		}
+		else if (CurrentOperation == EOperation::Add)
+		{
+			ModifyStat(Consequence.Target, Consequence.Value);
+		}
+		else if (CurrentOperation == EOperation::Subtract)
+		{
+			ModifyStat(Consequence.Target, -Consequence.Value);
+		}
+		else if (CurrentOperation == EOperation::Multiply)
+		{
+			float newValue = GetStat(Consequence.Target) * Consequence.Value;
+			SetStat(Consequence.Target, newValue);
+		}
+		break;
+
+	case EConsequenceType::ModifyRelationship:
+		if (CurrentOperation == EOperation::Set)
+		{
+			SetRelationship(Consequence.Target, Consequence.Value);
+		}
+		else if (CurrentOperation == EOperation::Add)
+		{
+			ModifyRelationship(Consequence.Target, Consequence.Value);
+		}
+		else if (CurrentOperation == EOperation::Subtract)
+		{
+			ModifyRelationship(Consequence.Target, -Consequence.Value);
+		}
+		else if (CurrentOperation == EOperation::Multiply)
+		{
+			float newValue = GetRelationship(Consequence.Target) * Consequence.Value;
+			SetRelationship(Consequence.Target, newValue);
+		}
+		break;
+
+	default:
+		UE_LOG(LogTemp, Warning, TEXT("GameStateManager: Type of consequence is invalid."));
+	}
+}
+
+bool AGameStateManager::HasStat(FName StatName) const
+{
+	return Stats.Contains(StatName);
+}
--- Source/OTW_BabyProject/Public/Narrative/GameStateManager.h
+++ Source/OTW_BabyProject/Public/Narrative/GameStateManager.h
@@ -4,11 +4,41 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameStateBase.h"
+#include "Types/GameTypes.h"
 #include "GameStateManager.generated.h"
+
 
 
 UCLASS()
 class OTW_BABYPROJECT_API AGameStateManager : public AGameStateBase
 {
 	GENERATED_BODY()
+	
+	TMap<FName, bool> Flags;
+	TMap<FName, float> Stats;
+	TMap<FName, float> Relationships;
+
+public:
+
+	// GETTERS
+	bool GetFlag(FName FlagName) const;
+	float GetStat(FName StatName) const;
+	float GetRelationship(FName CharacterName) const;
+
+	//SETTERS
+	void SetFlag(FName FlagName, bool Value);
+	void SetStat(FName StatName, float Value);
+	void SetRelationship(FName CharacterName, float Value);
+
+	//MODIFIERS
+	void ModifyStat(FName StatName, float Delta);
+	void ModifyRelationship(FName CharacterName, float Delta);
+
+	//CONSEQUENCES
+	UFUNCTION(BlueprintCallable)
+	void ApplyConsequence(const FChoiceConsequence& Consequence);
+
+	//TOOLS
+	UFUNCTION(BlueprintCallable)
+	bool HasStat(FName StatName) const;
 };
